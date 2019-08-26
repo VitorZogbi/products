4f7c2de6aa09a815c97814098815c6d8160c31ac
Vitor Jacob Braga Zogbi Filho <vitor.zogbi_BOLS@pampa.compasso>
2019-08-26 16:06:43 -0300

    db ok

--- src/app.js
+++ src/app.js
@@ -1,5 +1,37 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 const app = express();
+
+// Database
+const MONGO_LOCAL = 'mongodb://localhost/products';
+
+mongoose.connect(MONGO_LOCAL, {
+    useNewUrlParser: true
+})
+
+const db = mongoose.connection;
+
+db.on('connected', () => {
+    console.log('Mongoose default connection is open');
+});
+
+db.on('error', err => {
+    console.log(`Mongoose default connection has occured \n${err}`);
+});
+
+db.on('disconnected', () => {
+    console.log('Mongoose default connection is disconnected');
+});
+
+process.on('SIGINT', () => {
+    db.close(() => {
+        console.log(
+            'Mongoose default connection is disconnected due to application termination'
+        );
+        process.exit(0);
+    });
+});
 
 const indexRoutes = require('./routes/index-routes');
 app.use('/', indexRoutes);
