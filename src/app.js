60a5599eba4cadec161cbb98b5551225cdd30c29
Vitor Jacob Braga Zogbi Filho <vitor.zogbi_BOLS@pampa.compasso>
2019-09-25 18:01:19 -0300

    ok

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 require('./database');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json')
 
 //app routes
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const indexRoutes = require('./routes/index-routes');
 app.use('/', indexRoutes);
